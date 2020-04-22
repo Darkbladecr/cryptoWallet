e320407141b9d768d20b2800cedbc61bb45f8e97
Darkbladecr <smig88@gmail.com>
2020-04-22 08:47:51 +0100

    docker setup

--- api/index.js
+++ api/index.js
@@ -1,1 +1,14 @@
-module.exports = (req, res) => res.send('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = express.Router();
+
+router.all('/', (_, res) => res.send('Hello World!'));
+router.all('/wallet', require('./wallet'));
+router.all('/binanceTickers', require('./binanceTickers'));
+
+app.use('/api', router);
+
+app.listen(port, () =>
+  console.log(`cryptowallet-api listening at http://localhost:${port}`)
+);
