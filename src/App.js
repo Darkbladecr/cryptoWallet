2833945d4b768c2875979e711d517079220d0650
Darkbladecr <smig88@gmail.com>
2020-09-13 19:50:10 +0100

    update investment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const gbpFormatter = new Intl.NumberFormat('en-UK', {
   currency: 'GBP',
 });
 
-const walletPcnt = 1141 / 24141;
+const walletPcnt = 1141 / 26141;
 const investment = 1141;
 
 function App() {
